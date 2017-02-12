9cfb24c8ca651db1da601532ff0ab18006d6a5e6
Stig Murberg <stig.murberg@gmail.com>
2017-02-12 13:53:25 +0100

    Added some tests

--- web/models/video.ex
+++ web/models/video.ex
@@ -11,8 +11,8 @@ defmodule Rumbl.Video do
     timestamps()
   end
 
-  @required_fields ~w(url title description)
-  @optional_fields ~w(category_id)
+  # @required_fields ~w(url title description)
+  # @optional_fields ~w(category_id)
 
   @doc """
   Builds a changeset based on the `struct` and `params`.
