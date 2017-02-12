a92dcfad36e0e521cb95a55629de23c4d53c7259
Stig Murberg <stig.murberg@gmail.com>
2017-02-12 23:39:31 +0100

    More tests

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -1,5 +1,6 @@
 defmodule Rumbl.VideoControllerTest do
   use Rumbl.ConnCase
+  alias Rumbl.Video
 
   test "requires user auth for all actions", %{conn: conn} do
     Enum.map([
@@ -36,5 +37,46 @@ defmodule Rumbl.VideoControllerTest do
     assert String.contains?(conn.resp_body, user_video.title)
     refute String.contains?(conn.resp_body, other_video.title)
   end
+
+  @invalid_attrs %{title: "invalid"}
+  @valid_attrs %{url: "http://youtu.be", title: "vid", description: "a vid"}
+
+  defp video_count(query), do: Repo.one(from v in query, select: count(v.id))
+
+  @tag login_as: "foo"
+  test "creates user videos and redirects", %{ conn: conn, user: user } do
+    conn = post conn, video_path(conn, :create), video: @valid_attrs
+    assert redirected_to(conn) == video_path(conn, :index)
+    assert Repo.get_by!(Video, @valid_attrs).user_id === user.id
+  end
+
+  @tag login_as: "foo"
+  test "does not create video and renders error when invalid", %{ conn: conn } do
+    count_before = video_count(Video)
+    conn = post conn, video_path(conn, :create), video: @invalid_attrs
+    assert html_response(conn, 200) =~ ~r/check the errors/
+    assert video_count(Video) == count_before
+  end
+
+  @tag login_as: "foo"
+  test "authorizes actions against access by other users", %{ user: owner, conn: conn } do
+    video = insert_video(owner, @valid_attrs)
+    non_owner = insert_user(%{username: "sneaky"})
+    conn = assign(conn, :current_user, non_owner)
+
+    assert_error_sent :not_found, fn ->
+      get(conn, video_path(conn, :show, video))
+    end
+    assert_error_sent :not_found, fn ->
+      get(conn, video_path(conn, :edit, video))
+    end
+    assert_error_sent :not_found, fn ->
+      get(conn, video_path(conn, :update, video, video: @valid_attrs))
+    end
+    assert_error_sent :not_found, fn ->
+      get(conn, video_path(conn, :delete, video))
+    end
+  end
+
 
 end
